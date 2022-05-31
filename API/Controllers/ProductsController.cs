f24caf5dddbee08c7005dc309a4b0510b17af935
Nick Peachey <nick.peachey@gmail.com>
2022-05-31 10:45:27 +0100

    added products controller

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(StoreContext context)
+        {
+
+        }
+    }
+}
