d7c4ff39b979eff1b3e9e2bcadc5a77a24fe3221
Nick Peachey <nick.peachey@gmail.com>
2022-05-31 08:46:12 +0100

    added db init

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "product name etc",
+                    Description = "some description"
+                }
+            };
+        }
+    }
+}
