bb5a7171ae325ac315a840ae21069ed58e6e8a49
Nick Peachey <nick.peachey@gmail.com>
2022-06-01 11:19:41 +0100

    LTBECSWNRR-4 styled product card more

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -8,25 +8,33 @@ interface Props {
 export default function ProductCard ({product}: Props){
     return(
         <Card>
-        <CardMedia
-          component="img"
-          height="140"
-          image="http://picsum.photos/200"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+            <CardHeader avatar={
+                <Avatar sx={{bgcolor: 'secondary.main'}}>
+                    {product.name.charAt(0).toUpperCase()}
+                </Avatar>
+            }
+            title={product.name}
+            titleTypographyProps={{
+                sx: {fontWeight: 'bold', color: 'primary.main'}
+            }}
+            />
+            <CardMedia
+            sx={{height: 140, backgroundSize: 'contain', bgcolor: 'primary.light'}} 
+            image={product.pictureUrl}
+            title={product.name}
+            />
+            <CardContent>
+            <Typography color="secondary" gutterBottom variant="h5">
+                ${(product.price / 100).toFixed(2)}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+               {product.brand} / {product.type}
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Add to cart</Button>
+            <Button size="small">View</Button>
+            </CardActions>
       </Card>
     )
 }
