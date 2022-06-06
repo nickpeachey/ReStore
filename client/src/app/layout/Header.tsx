052172424bbbf0d7c68c6f81ae7e79efdfa6d777
Nick Peachey <nick.peachey@gmail.com>
2022-06-06 17:00:59 +0100

    added box

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,11 +1,12 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { NavLink } from "react-router-dom";
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
-}
+}8
 
 const midLinks = [
     {title: 'catalog', path: '/catalog'},
@@ -18,24 +19,41 @@ const rightLinks = [
     {title: 'register', path: '/register'}
 ]
 
+const navStyles = {
+    textDecoration: 'none',
+    color: 'inherit', 
+    typography: 'h6',
+    '&:hover' : {
+    color: 'grey.500'
+    },
+    '&.active' : {
+    color: 'text.secondary'
+    }
+}
+
 export default function Header({darkMode, handleThemeChange}: Props) {
     return (
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography sx={{color: 'inherit', textDecoration:'none'}} variant="h6" component={NavLink} to='/'>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems:'center'}}>
+                <Box display='flex' alignItems='center'>
+                <Typography sx={navStyles} variant="h6" component={NavLink} to='/'>
                     Re-Store
                 </Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange}/>
+                </Box>
+
                 <List sx={{display: 'flex'}}>
                     {midLinks.map(({title, path}) => (
                         <ListItem component={NavLink} 
                         to={path} 
                         key={path}
-                        sx={{color: 'inherit', typography: 'h6'}}>
+                        sx={navStyles}
+                        >
                             {title.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
+                <Box display='flex' alignItems='center'>
                 <IconButton size='large' sx={{color: 'white'}}>
                     <Badge badgeContent={4} color='secondary'>
                         <ShoppingCart />
@@ -46,11 +64,13 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                         <ListItem component={NavLink} 
                         to={path} 
                         key={path}
-                        sx={{color: 'inherit', typography: 'h6'}}>
+                        sx={navStyles}>
                             {title.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
+                </Box>
+
             </Toolbar>
         </AppBar>
     )
