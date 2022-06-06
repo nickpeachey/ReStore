49db53d07238992971dd738d15089be8aa1f92fb
Nick Peachey <nick.peachey@gmail.com>
2022-06-06 15:26:43 +0100

    added router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,12 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { useState } from 'react';
+import {  Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 
 function App() {
@@ -25,7 +31,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/catalog' element={<Catalog />}/>
+          <Route path='/catalog/:id' element={<ProductDetails />}/>
+          <Route path='/about' element={<AboutPage />}/>
+          <Route path='/contact' element={<ContactPage />}/>
+        </Routes>
       </Container>
     </ThemeProvider>
   );
