8e3343732b93c2ce475f61a0b29c1c4ee5db0ce4
Nick Peachey <nick.peachey@gmail.com>
2022-06-27 15:38:03 +0100

    added error list

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
 import agent from "../../app/api/agent";
-import { Product } from "../../app/models/product"
+import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
 export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-    const [products, setProducts] = useState<Product[]>([])
+  useEffect(() => {
+    agent.Catalog.list().then((data) => setProducts(data));
+  }, []);
 
-
-    useEffect(() => {
-      agent.Catalog.list()
-        .then(data => setProducts(data))
-    },[])
-  
-    return (
-        <>
-        <ProductList products={products} />
-        </>
-    )
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
 }
