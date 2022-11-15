6a1660b841f220b85a427f3ce8081fc9a6ccc57e
Nick Peachey <nick.peachey@gmail.com>
2022-11-15 15:43:32 +0000

    added account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly UserManager<User> _userManager;
+
+    public AccountController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<User>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByNameAsync(loginDto.Username);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            return Unauthorized();
+        return user;
+    }
+}
