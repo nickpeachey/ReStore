b80a2dc5a01a53335c366c76827afd935bb54d01
Nick Peachey <nick.peachey@gmail.com>
2022-11-15 09:15:49 +0000

    end of aded filters and pagination

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,4 @@
-import {
-  Box,
-  FormLabel,
-  Grid,
-  Pagination,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { FormLabel, Grid, Paper } from "@mui/material";
 import { useEffect } from "react";
 import AppPagination from "../../app/components/AppPagination";
 import CheckboxButtons from "../../app/components/CheckboxButtons";
@@ -16,6 +9,7 @@ import {
   fetchFiltersAsync,
   fetchProductsAsync,
   productSelectors,
+  setPageNumber,
   setProductParams,
 } from "./catalogSlice";
 import ProductList from "./ProductList";
@@ -31,7 +25,6 @@ export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
   const {
     productsLoaded,
-    status,
     filtersLoaded,
     brands,
     types,
@@ -48,11 +41,11 @@ export default function Catalog() {
     if (!filtersLoaded) dispatch(fetchFiltersAsync());
   }, [dispatch, filtersLoaded]);
 
-  if (status.includes("pending") || !metaData) return <LoadingComponent />;
+  if (!filtersLoaded) return <LoadingComponent message="Loading products..." />;
 
   return (
     <>
-      <Grid container spacing={4}>
+      <Grid container columnSpacing={3}>
         <Grid item xs={3}>
           <Paper sx={{ mb: 2 }}>
             <ProductSearch />
@@ -92,13 +85,15 @@ export default function Catalog() {
           <ProductList products={products} />
         </Grid>
         <Grid item xs={3} />
-        <Grid item xs={9}>
-          <AppPagination
-            metaData={metaData}
-            onPageChange={(page: number) =>
-              dispatch(setProductParams({ pageNumber: page }))
-            }
-          />
+        <Grid item xs={9} sx={{ mb: 2 }}>
+          {metaData && (
+            <AppPagination
+              metaData={metaData}
+              onPageChange={(page: number) =>
+                dispatch(setPageNumber({ pageNumber: page }))
+              }
+            />
+          )}
         </Grid>
       </Grid>
     </>
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -24,8 +24,8 @@ function getAxiosParams(productParams: ProductParams) {
     params.append('orderBy', productParams.orderBy);
 
     if (productParams.searchTerm) params.append('searchTerm', productParams.searchTerm);
-    if (productParams.brands) params.append('brands', productParams.brands.toString());
-    if (productParams.types) params.append('types', productParams.types.toString());
+    if (productParams.brands?.length > 0) params.append('brands', productParams.brands.toString());
+    if (productParams.types?.length > 0) params.append('types', productParams.types.toString());
 
     return params;
 }
@@ -74,7 +74,9 @@ export const fetchFiltersAsync = createAsyncThunk(
     return {
         pageNumber: 1,
         pageSize: 6,
-        orderBy: 'name'
+        orderBy: 'name',
+        brands: [],
+        types: []
     }
   }
 
@@ -92,6 +94,10 @@ export const catalogSlice = createSlice({
     }),
     reducers: {
         setProductParams: (state, action) => {
+            state.productsLoaded = false;
+            state.productParams = {...state.productParams, ...action.payload, pageNumber: 1};
+        },
+        setPageNumber: (state, action) => {
             state.productsLoaded = false;
             state.productParams = {...state.productParams, ...action.payload};
         },
@@ -141,4 +147,4 @@ export const catalogSlice = createSlice({
 })
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
-export const {setProductParams, resetProductParams, setMetaData} = catalogSlice.actions;
+export const {setProductParams, resetProductParams, setMetaData, setPageNumber} = catalogSlice.actions;
