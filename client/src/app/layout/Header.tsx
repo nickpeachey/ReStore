79935c926a2a8c6149d62e0b48aa7d6116c1cba0
Nick Peachey <nick.peachey@gmail.com>
2022-11-18 16:41:37 +0000

    started to add login

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -12,6 +12,7 @@ import {
 import { Box } from "@mui/system";
 import { Link, NavLink, useHistory } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
   darkMode: boolean;
@@ -44,6 +45,7 @@ const navStyles = {
 export default function Header({ darkMode, handleThemeChange }: Props) {
   const history = useHistory();
   const { basket } = useAppSelector((state) => state.basket);
+  const { user } = useAppSelector((state) => state.account);
   const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
@@ -79,13 +81,22 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
               <ShoppingCart />
             </Badge>
           </IconButton>
-          <List sx={{ display: "flex" }}>
-            {rightLinks.map(({ title, path }) => (
-              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+          {user ? (
+            <SignedInMenu />
+          ) : (
+            <List sx={{ display: "flex" }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
